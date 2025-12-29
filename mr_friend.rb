9ca935f640f222fbd332b9c76269dee81f2ac6c7
Caleb Stone <thebadmrbiss@proton.me>
2025-12-28 21:12:51 -0500

    Add stock data retrieval method and prompts (#3)

--- mr_friend.rb
+++ mr_friend.rb
@@ -1,1 +1,30 @@
-puts "Hello World!"
+Stock_Database = {
+    'CHWY' => { name: "Chewy, Inc.", price: 32.00,},
+    'CELH' => { name: "Celsius Holdings, Inc.", price: 45.50},
+    'PFE' => { name: "Pfizer Inc.", price: 25.00},
+}
+
+def get_stock_data(symbol, data)
+    data[symbol.upcase]
+end
+
+if __FILE__ == $0
+  puts "Welcome to Mr Friend Stock Analyzer!"
+
+  loop do
+    print "Please enter a stock symbol to retrieve data \n: "
+    input = gets.chomp.strip.upcase
+
+    result = get_stock_data(input, Stock_Database)
+
+    if result
+      puts "Stock Name: #{result[:name]}, Price: #{result[:price]}"
+          # Break after input for now
+          break
+    else
+      puts "Error: Stock symbol not found."
+      # Break after input for now
+      break
+    end
+  end
+end

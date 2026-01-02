724f6a1a90cdb1a569af9f09d370b99fc130e78e
Caleb Stone <thebadmrbiss@proton.me>
2026-01-01 20:41:34 -0500

    Add PE ratio and EPS (#7)

--- mr_friend.rb
+++ mr_friend.rb
@@ -1,7 +1,7 @@
 STOCK_DATABASE = {
-    'CHWY' => { name: "Chewy, Inc.", price: 32.00,},
-    'CELH' => { name: "Celsius Holdings, Inc.", price: 45.50},
-    'PFE' => { name: "Pfizer Inc.", price: 25.00},
+    'CHWY' => { name: "Chewy, Inc.", price: 32.00, pe: 45.2, eps: 0.71 },
+    'CELH' => { name: "Celsius Holdings, Inc.", price: 45.50, pe: 60.1, eps: 0.76 },
+    'PFE' => { name: "Pfizer Inc.", price: 25.00, pe: 22.5, eps: 1.11 },
 }
 
 def get_stock_data(symbol, data)
@@ -18,7 +18,7 @@ if __FILE__ == $0
     result = get_stock_data(input, STOCK_DATABASE)
 
     if result
-      puts "Stock Name: #{result[:name]}, Price: #{result[:price]}"
+      puts "Stock Name: #{result[:name]}, Price: #{result[:price]}, P/E Ratio: #{result[:pe]}, EPS: #{result[:eps]}"
           # Break after input for now
           break
     else

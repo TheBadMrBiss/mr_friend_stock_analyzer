b2c267561d926fa21bbe5ad4a61ed90ec0c48602
TheBadMrBiss <thebadmrbiss@proton.me>
2026-01-01 20:25:25 -0500

    Add PE ratio and EPS stock data

--- mr_friend.rb
+++ mr_friend.rb
@@ -1,7 +1,7 @@
 STOCK_DATABASE = {
-    'CHWY' => { name: "Chewy, Inc.", price: 32.00,},
-    'CELH' => { name: "Celsius Holdings, Inc.", price: 45.50},
-    'PFE' => { name: "Pfizer Inc.", price: 25.00},
+    'CHWY' => { name: "Chewy, Inc.", price: 32.00, pe: 45.2, eps: 0.71 },
+    'CELH' => { name: "Celsius Holdings, Inc.", price: 45.50, pe: 60.1, eps: 0.76 },
+    'PFE' => { name: "Pfizer Inc.", price: 25.00, pe: 22.5, eps: 1.11 },
 }
 
 def get_stock_data(symbol, data)

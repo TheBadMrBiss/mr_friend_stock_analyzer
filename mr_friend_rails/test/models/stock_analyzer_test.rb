f162ce3db2ecf755ce37b5bae84fe15384b6e4f3
TheBadMrBiss <thebadmrbiss@proton.me>
2026-01-23 16:51:15 -0500

    Add initial stock analyzer tests

---
+++ mr_friend_rails/test/models/stock_analyzer_test.rb
@@ -0,0 +1,18 @@
+require "test_helper"
+
+class StockAnalyzerTest < ActiveSupport::TestCase
+  test "should return correct data for known symbol" do
+    analyzer = StockAnalyzer.new
+    result = analyzer.lookup("CHWY")
+
+    assert_equal "Chewy Inc.", result[:name]
+    assert_equal 32.50, result[:price]
+  end
+
+  test "should return error for unknown symbol" do
+    analyzer = StockAnalyzer.new
+    result = analyzer.lookup("INVALID")
+
+    assert_equal "Symbol not found", result[:error]
+  end
+end
